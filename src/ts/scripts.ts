d035bcd4b522abe42877beb1069cba3393377c5d
Roman Opalko <roman.opalko@2sic.com>
2021-09-17 13:53:40 +0200

    remove jquery, clean up, refactoring

--- src/ts/scripts.ts
+++ src/ts/scripts.ts
@@ -1,27 +1,78 @@
-$(function() {
-  var navHeight = $('header').height();
+document.addEventListener("DOMContentLoaded", (event) => {
+	// get navHight correct scrollposition
+	var navHeight = document.getElementsByTagName('header')[0].offsetHeight;
+
 	// attach click to all accordions when loading
-	$('.co-accordion-title').click(function(e){
-		e.preventDefault();
+	var accordionOpener = document.querySelectorAll('.co-accordion-title');
 
-    // add hash to url
-		location.hash = $(this).data('hash');
+	accordionOpener.forEach((elem: HTMLElement, index) => {		
+		elem.addEventListener('click', (event) => {
+			event.preventDefault();
 
-    $(this).toggleClass('active').next('.co-accordion-content').slideToggle('normal');
+			const currentElem = event.currentTarget as HTMLElement;
+			const parent = currentElem.parentElement;			
+
+			// add hash to url
+			location.hash = currentElem.dataset.hash;
+
+			// open / close mechanic for slide
+			if (!parent.classList.contains('active')) {
+				openAccordion(currentElem);        
+			} else {
+				closeAccordion(currentElem)
+			}
+		})
 	});
 
 	// get hash from url and open specific item
 	if(window.location.hash){
-    var hash = window.location.hash.replace('#', '');
-    var targetHashElem = $(`#hash-${hash}`);
+		const hash = window.location.hash.replace('#', '');
+		const targetHashElem = document.getElementById(`hash-${hash}`);
 		
-		if(targetHashElem.length > 0){
-      targetHashElem.click();
-      $('html, body').animate({
-        scrollTop: targetHashElem.offset().top - navHeight 
-      }, 500, function() {
-				targetHashElem.addClass('active').next('.co-accordion-content').slideDown('normal')
-			}); 
+		// if target element exists scroll to element
+		if(targetHashElem){
+			const elemOffsetX = targetHashElem.getBoundingClientRect().top + window.scrollY - navHeight
+			
+			window.scrollTo({
+				top: elemOffsetX,
+				left: 0,
+				behavior: 'smooth'
+			});
+
+			openAccordion(targetHashElem);         
 		}
+	}
+
+	function openAccordion(elem: HTMLElement) {
+		const parent = elem.parentElement;			
+		const sibling = elem.nextElementSibling as HTMLElement;
+		
+		parent.classList.add('active');
+		sibling.style.height = 'auto';
+
+		let height = sibling.clientHeight + "px";
+
+		sibling.style.height = '0px';
+
+		setTimeout(() => {
+			sibling.style.height = height;
+		}, 0);
+	}
+
+	function closeAccordion(elem: HTMLElement) {
+		const parent = elem.parentElement;			
+		const sibling = elem.nextElementSibling as HTMLElement;
+
+		sibling.style.height = sibling.clientHeight + "px";
+
+		setTimeout(() => {
+			sibling.style.height = '0px';
+		}, 0)
+
+		sibling.addEventListener('transitionend', function () {
+			parent.classList.remove('active');
+		}, {
+			once: true
+		});
 	}
 });
