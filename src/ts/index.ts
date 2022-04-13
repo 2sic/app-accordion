35e4639d4f8855efa85cc3fc35a79be204f14d19
maaaximum <Maxim.Huesler@2sic.com>
2022-04-13 09:52:43 +0200

    added bs5 accordion component

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,7 @@
 import { initAccordion } from './lib-2sxc-accordion';
+import { initAccordionBs5 } from './lib-2sxc-bs5-accordion';
 
 var winAny = window as any;
 winAny.appAccordion4 ??= {};
 winAny.appAccordion4.init ??= initAccordion;
+winAny.appAccordion4.initBs5 ??= initAccordionBs5;
